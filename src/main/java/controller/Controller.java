4d7c62c30c7095f5ba3bdc04a8ef6452a30fe263
Владимир <vkuznecov1504@mail.ru>
2024-05-14 09:15:30 +0400

    Написали сервис, который собирает корзину для интернет магазина

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,34 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import service.OrderService;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/order")
+public class Controller {
+
+    private OrderService orderService;
+
+
+    public void ItemsController(OrderService orderService) {
+
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/add")
+    public List<Integer> addItems(@RequestParam List<Integer> ids) {
+
+        return orderService.addItems(ids);
+    }
+
+    @GetMapping("/get")
+    public List<Integer> getItems() {
+        return orderService.getItems();
+    }
+}
